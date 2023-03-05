cbd436dd2df1de767a4c3d3d3254dd1b59125d42
Stryx7 <rerison.stryx@hotmail.com>
2023-03-05 19:07:58 -0300

    Criação de Questionário

---
+++ teste2.js
@@ -0,0 +1,19 @@
+let nome = prompt("Qual seu nome");
+let idade = prompt("Quantos anos você tem?");
+let linguagem = prompt("Qual linguagem de programação você está estudando");
+
+alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`);
+
+let opcao = prompt(
+  `Está gosta de estudar ${linguagem}? Responda: 1 SIM, 2 NÃO`
+);
+
+while (opcao == 1 || opcao == 2) {
+  if (opcao == 1) {
+    alert("Muito bom! Continue estudando e você terá muito sucesso!");
+    break; 
+  } else if (opcao == 2) {
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+    break;  
+  } 
+}
